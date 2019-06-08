6407ab43b841e09d4a5b9ff32064679ef5ea13dc
buley004 <colinbuley@D-10-18-181-31.dhcp4.washington.edu>
2019-06-08 11:46:59 -0700

    adding sql schema

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,15 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS mediator;
+CREATE DATABASE mediator;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+
+CREATE TABLE `photos` (
+	`id` INT( 11 ) AUTO_INCREMENT NOT NULL,
+	`photoPath` VARCHAR(55) NOT NULL,
+	`wins` INT NOT NULL SET DEFAULT 0,
+	`losses` INT NOT NULL SET DEFAULT 0,
+	`category` VARCHAR(20),
+  'pair' BOOLEAN SET DEFAULT false,
+  'pairPath' VARCHAR(55),
+  'userId' VARCHAR(55),
+	PRIMARY KEY ( `id` )
+);
