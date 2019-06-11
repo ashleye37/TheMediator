e21c0e7ef7db3bdd8b9bc6102ae141a1621dfe09
Ashley Evans <ashley_e@live.com>
2019-06-10 17:43:10 -0700

    Removing previous stalk images, adding new paired images, and created files needed for using passport for Google Authentication.

---
+++ routes/auth-routes.js
@@ -0,0 +1,20 @@
+var router = require("express").Router();
+
+// Set up login route
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+// Set up logout route
+router.get("/logout", (req, res) => {
+  // Handle with passport
+  res.send("logging out");
+});
+
+// Set up login with Google
+router.get("/google", (req, res) => {
+  // Handle with passport
+  res.send("Logging in with Google");
+});
+
+module.exports = router;
