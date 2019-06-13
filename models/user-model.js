6eb2b536ebe5ddb78353ab6982e72862d946b428
buley004 <colinbuley@D-10-19-115-235.dhcp4.washington.edu>
2019-06-12 20:16:11 -0700

    changing sequelize datatype for users

--- models/user-model.js
+++ models/user-model.js
@@ -1,9 +1,9 @@
 // var Sequelize = require("sequelize");
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("user", {
-    username: DataTypes.String,
-    googleId: DataTypes.String,
-    thumbnail: DataTypes.String
+    username: DataTypes.STRING,
+    googleId: DataTypes.STRING,
+    thumbnail: DataTypes.STRING
   });
   return User;
 };
