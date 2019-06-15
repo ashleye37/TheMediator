b409c9d29c325f5f851524a5f6c85d876d825d59
zenwattage <zenwattage@gmail.com>
2019-06-15 11:01:13 -0700

    commented out /example/:id

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -12,13 +12,13 @@ module.exports = function(app) {
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
+  /* app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
       res.render("example", {
         example: dbExample
       });
     });
-  });
+  }); */
 
   //NEED A PAGE AND ROUTE VOTE PAGE
   //POPULATE VOTE PAGE
