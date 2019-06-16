7f1474d4395958a172104036cc7016138dd7c462
buley004 <colinbuley@Colins-MacBook-Pro.local>
2019-06-16 16:49:32 -0700

    added post call

--- public/js/battle.js
+++ public/js/battle.js
@@ -1,6 +1,15 @@
 //call post route
 $("#submit-form").on("submit", function(){
   var urls = {
-    primaryPath: 
+    primaryPath: $("#url").val(),
+    secondPath: $("#url2").val()
   };
+
+  $.ajax({
+    type: "POST",
+    url: "api/photos",
+    data: urls
+  }).then(function(){
+    alert("Successfully submitted!")
+  });
 })
